eff150074c89ef801cd129f72a0abe9551ea36b4
T-O-R-U-S <bageliq@protonmail.com>
2022-12-21 12:52:31 +0400

    Fixed broken rendering

--- src/main/kotlin/torus/insomniometer/client/Client.kt
+++ src/main/kotlin/torus/insomniometer/client/Client.kt
@@ -1,15 +1,18 @@
 package torus.insomniometer.client
 
 import com.mojang.blaze3d.systems.RenderSystem
+
 import net.fabricmc.api.Environment
 import net.fabricmc.api.EnvType
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
+
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.DrawableHelper
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
-
-
+import net.minecraft.stat.Stats
+import net.minecraft.world.Difficulty
+import net.minecraft.world.GameRules
 
 var clientState = 0
 
@@ -19,11 +22,42 @@ class Client : HudRenderCallback {
     val PHANTOM_RED = Identifier(torus.insomniometer.MOD_ID, "textures/insomnia/phantom_red.png")
 
     // Instead of performing all these checks every frame, the state changes should be delegated to an external function
+    // that is scheduled for every tick
     override fun onHudRender(matrixStack: MatrixStack?, tickDelta: Float) {
+        val client = MinecraftClient.getInstance() ?: return
 
+        val server = client.server ?: return
         val window = MinecraftClient.getInstance()?.window ?: return
+        val world = client.world ?: return
+        val player = client.player ?: return
 
-//        val stats = player.statHandler
+        // Getting the `client.player` directly doesn't seem to update it unless you open the stats screen.
+        // This is why I need the server's player stats object.
+        val serverStats = server.playerManager.getPlayer(player.gameProfile.name)?.statHandler ?: return
+
+        val timeSinceRest = serverStats.getStat(Stats.CUSTOM, Stats.TIME_SINCE_REST)
+
+        // If insomnia doesn't apply, don't render the alert.
+        if (!server.gameRules.get(GameRules.DO_INSOMNIA).get())
+            return
+        // If phantoms can't spawn, don't render the alert
+        if (world.difficulty == Difficulty.PEACEFUL || !world.gameRules.get(GameRules.DO_MOB_SPAWNING).get())
+            return
+        // If the player can't be damaged, don't render the alert.
+        if(player.abilities.invulnerable)
+            return
+
+        if (timeSinceRest < 60000) {
+            return
+        }
+
+        // Green-eyed phantom
+        RenderSystem.setShaderTexture(0, PHANTOM_GREEN)
+
+        // If phantoms are very close to actively spawning, turn the phantom's eyes red
+        if (timeSinceRest > 71000) {
+            RenderSystem.setShaderTexture(0, PHANTOM_RED)
+        }
 
         val horizontalCenter = window.scaledWidth / 2
         val verticalBottom = window.scaledHeight
@@ -40,7 +74,7 @@ class Client : HudRenderCallback {
 
         DrawableHelper.drawTexture(
                 matrixStack,
-                horizontalCenter-110,
+                horizontalCenter + 110,
                 verticalBottom - 18,
                 16,
                 16,
@@ -51,19 +85,5 @@ class Client : HudRenderCallback {
                 16,
                 16
         )
-
-//        for (i in 0..10) {
-//            DrawableHelper.drawTexture(
-//                    matrixStack,
-//                    horizontal_center-94 + (i*9), // X position
-//                    vertical_bottom-54, // Y position
-//                    0f, // u
-//                    0f, // v
-//                    16, //width
-//                    16, // height
-//                    16, // textureWidth
-//                    16 // textureHeight
-//            )
-//        }
     }
 }
